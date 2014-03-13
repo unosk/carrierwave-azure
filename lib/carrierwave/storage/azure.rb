ac5fa73d45c8bc44ded54624063e8e935fb6dbc3
Mike Bajur <mike@vbot.tv>
2014-03-13 11:01:32 +0100

    Send file in 4MB chunk

--- lib/carrierwave/storage/azure.rb
+++ lib/carrierwave/storage/azure.rb
@@ -32,9 +32,20 @@ module CarrierWave
         end
 
         def store!(file)
-          @content = file.read
           @content_type = file.content_type
-          @connection.create_block_blob @uploader.azure_container, @path, @content, content_type: @content_type
+          file_to_send  = ::File.open(file.file, 'rb')
+          blocks        = []
+          i = 0
+
+          until file_to_send.eof?
+            i += 1
+            @content = file_to_send.read 4194304 # Send 4MB chunk
+            @connection.create_blob_block @uploader.azure_container, @path, i.to_s, content
+            blocks << i.to_s
+          end
+
+          @connection.commit_blob_blocks @uploader.azure_container, @path, blocks
+
           true
         end
 
