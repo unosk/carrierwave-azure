551524b73107dcc6600420607546c77a306f1629
unosk <yusuke.shibahara@heathrow.co.jp>
2015-01-24 15:22:24 +0900

    Fix should => expect on RSpec

--- spec/carrierwave-azure_spec.rb
+++ spec/carrierwave-azure_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe CarrierWave::Uploader::Base do
   it 'should define azure as a storage engine' do
-    described_class.storage_engines[:azure].should == 'CarrierWave::Storage::Azure'
+    expect(described_class.storage_engines[:azure]).to eq 'CarrierWave::Storage::Azure'
   end
 
   it 'should define azure options' do
-    should respond_to(:azure_storage_account_name)
-    should respond_to(:azure_storage_access_key)
-    should respond_to(:azure_storage_blob_host)
-    should respond_to(:azure_container)
+    is_expected.to respond_to(:azure_storage_account_name)
+    is_expected.to respond_to(:azure_storage_access_key)
+    is_expected.to respond_to(:azure_storage_blob_host)
+    is_expected.to respond_to(:azure_container)
   end
 end
